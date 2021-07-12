14b6bc4eb779db720e50a06a6901f83858d7717b
Claret Nnamocha <devclareo@gmail.com>
2021-07-12 16:05:36 +0100

    gis

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,10 +1,12 @@
+export * as gis from "./gis";
 export * as jwt from "./jwt";
 export * as mail from "./mail";
 export * as payments from "./payments";
-import request from "./request";
-import response from "./response";
 export * as sms from "./sms";
 export * as storage from "./storage";
 export * as types from "./types";
+
+import request from "./request";
+import response from "./response";
 
 export { response, request };
