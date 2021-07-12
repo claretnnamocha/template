a257e0f47d6e9dd29d1dbb8c889f3b0142e92173
Claret Nnamocha <devclareo@gmail.com>
2021-07-12 14:03:28 +0100

    Update africastalking.ts

--- src/helpers/sms/africastalking.ts
+++ src/helpers/sms/africastalking.ts
@@ -6,19 +6,19 @@ const {
   AFRICASTALKING_SENDER_ID,
 } = process.env;
 
-const credentials = {
-  apiKey: AFRICASTALKING_APIKEY,
-  username: AFRICASTALKING_USERNAME,
-};
-
-const { SMS } = Africastalking(credentials);
-
 export const send = async (
   to: string,
   body: string,
   from: string = AFRICASTALKING_SENDER_ID
 ) => {
   try {
+    const credentials = {
+      apiKey: AFRICASTALKING_APIKEY,
+      username: AFRICASTALKING_USERNAME,
+    };
+
+    const { SMS } = Africastalking(credentials);
+
     const message = await SMS.send({ to, from, message: body });
 
     return message != null;
