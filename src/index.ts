4c683cee0f1e54ca57524a5f01a9a90f2a0f3a83
Claret Nnamocha <devclareo@gmail.com>
2021-07-12 13:21:15 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import express, { NextFunction, Response } from "express";
 import formdata from "express-form-data";
 import swaggerUi from "swagger-ui-express";
 import { env, swagger } from "./configs";
-import { response } from "./helpers";
-import { CustomRequest } from "./helpers/types/interfaces/request/others";
-// import routes from "./routes";
+// import { response } from "./helpers";
+// import { CustomRequest } from "./helpers/types/interfaces/request/others";
+import routes from "./routes";
 
 const app = express();
 const port: number = env.port;
@@ -17,15 +17,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagger.config));
-// app.use("", routes);
+app.use("", routes);
 
-app.use((err: Error, req: CustomRequest, res: Response, next: NextFunction) => {
-  return response(
-    res,
-    { status: false, message: `Internal server error [${err.message}]` },
-    500
-  );
-});
+// app.use((err: Error, req: CustomRequest, res: Response, next: NextFunction) => {
+//   return response(
+//     res,
+//     { status: false, message: `Internal server error [${err.message}]` },
+//     500
+//   );
+// });
 
 if (require.main) {
   app.listen(port, () => {
