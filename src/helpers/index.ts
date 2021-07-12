4e17f938145b379778c214386af55b331918d738
Claret Nnamocha <devclareo@gmail.com>
2021-07-12 13:27:10 +0100

    update

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,10 +1,10 @@
-import * as jwt from "./jwt";
-import * as mail from "./mail";
-import * as payments from "./payments";
-import request from "./request";
+// export * as jwt from "./jwt";
+// export * as mail from "./mail";
+// export * as payments from "./payments";
+// export request from "./request";
 import response from "./response";
-import * as sms from "./sms";
-import * as storage from "./storage";
-import * as types from "./types";
+// export * as sms from "./sms";
+// export * as storage from "./storage";
+export * as types from "./types";
 
-export { request, response, jwt, mail, sms, storage, types, payments };
+export { response };
