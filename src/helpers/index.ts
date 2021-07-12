bb34d86db8c432c11bf016f03f38eeb4bf37f9c6
Claret Nnamocha <devclareo@gmail.com>
2021-07-12 13:43:08 +0100

    Update index.ts

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,10 +1,10 @@
 export * as jwt from "./jwt";
 export * as mail from "./mail";
-// export * as payments from "./payments";
+export * as payments from "./payments";
 import request from "./request";
 import response from "./response";
-// export * as sms from "./sms";
-export * as storage from "./storage";
+export * as sms from "./sms";
+// export * as storage from "./storage";
 export * as types from "./types";
 
 export { response, request };
