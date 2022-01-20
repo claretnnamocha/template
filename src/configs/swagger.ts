878073ae894efe92456f85a2efc7e2f85da55978
claretnnamocha <devclareo@gmail.com>
2022-01-20 19:18:38 +0100

    updates

--- src/configs/swagger.ts
+++ src/configs/swagger.ts
@@ -1,17 +1,21 @@
 import swaggerJsDoc, { Options } from "swagger-jsdoc";
-import { port } from "./env";
+import { description, version, displayName } from "../../package.json";
+import { env, port } from "./env";
+
+let apis = ["./src/docs/*.yml"];
+if (env === "development") apis.push("./src/docs/*.yaml");
 
 const swagger: Options = {
   swaggerDefinition: {
     info: {
-      version: "1.0.0",
-      description: "Template backend",
-      title: "Template Backend API",
+      version,
+      description,
+      title: `${displayName} (${env})`,
       contact: { name: "Claret Nnamocha", email: "devclareo@gmail.com" },
       servers: [{ url: `http://localhost:${port}` }],
     },
   },
-  apis: ["./src/docs/*.yml"],
+  apis,
 };
 
 const config = swaggerJsDoc(swagger);
